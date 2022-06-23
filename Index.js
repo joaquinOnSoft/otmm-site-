ad513200cd682da7b8eea0fa4321a3c9570665ad
JoaquinOnSoft <joaquingp@gmail.com>
2022-06-23 13:08:20 +0200

    Initial version. Work in progress

---
+++ Index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+class Index {
+	static app = express();
+
+	static router = express.Router();
+
+	static main(){
+		dotenv.config();
+		Index.setUpServer();
+	}
+	
+	static async setUpServer(){
+		Index.app.use(cors());
+		Index.app.use(express.json());
+		//Index.app.use('/api/v1/movies', MoviesRoute.configRoutes(Index.router));
+		Index.app.use('*', (req, res) => {
+			res.status(404).json({error: 'not found'});
+		})
+
+		const port = process.env.PORT || 8000;
+
+		try{
+			Index.app.listen(port, () => {
+				console.log(`Server us running on port ${port}`);
+			});
+		} catch (e){
+			console.error(e);
+			process.exit(-1);
+		}
+	}
+}
+
+Index.main();
---
+++ otmm/OTMMAPI.js
@@ -0,0 +1,118 @@
+import axios from 'axios';
+import querystring from 'node:querystring';
+import 'dotenv/config';
+
+const urlBase = process.env.OTMM_API_URL;
+const user = process.env.OTMM_USER;
+const pass = process.env.OTMM_PASSWORD;
+
+class OTMMAPI{
+	
+	/**
+	 * <strong>Create a Session</strong>
+	 * Create a security Session in OTMM. It returns a valid SecuritySession
+	 * object if the provided credentials are valid. This is equivalent to login to OTMM
+	 * <ul>
+	 * 	<li>Method: POST</li>
+	 * 	<li>API method: /v6/sessions</li>
+	 * 	<li>URL example: https://developer.opentext.com/otmmapi/v6/sessions</li>
+	 * </ul>
+	 * <strong>NOTE:</strong>
+	 * With the exception of methods related to 'sessions', any call to an OTMM REST API
+	 * method must include the session id in the 'X-Requested-By' header.
+	 * @param user - User alias
+	 * @param pass - User password
+	 * @return session information
+	 * <code>
+	 * {
+	 *	  session_resource: {
+	 *		session: {
+	 *		  domain_name: 'OTMM',
+	 *		  id: 471542185,
+	 *		  local_session: false,
+	 *		  login_name: 'tsuper',
+	 *		  message_digest: 'b8271108836bef44130e71ee91bd51d4e75e2733',
+	 *		  role_name: 'Administrator',
+	 *		  user_full_name: 'admin, otmm',
+	 *		  user_id: '1001',
+	 *		  user_role_id: 1,
+	 *		  validation_key: -2045393682
+	 *		}
+	 *	  }
+	 *	}
+	 * </code>
+	 */
+	static async createSession(user, pass){
+		try {
+			let link = urlBase + "/v6/sessions";
+			console.log("URL: " + link);
+			
+			let payloadJSON  = {
+				"username": user,
+				"password": pass
+			};
+			let payload =  (new URLSearchParams(payloadJSON)).toString()
+
+			const resp = await axios.post(link, payload, {
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+				}
+			});
+			
+			return resp.data;
+		} catch (err) {
+			console.error(err);
+			return null;
+		}
+	}
+
+	static async get(session, url){
+		 return await axios.get(url, 
+				{
+					headers: {
+						"X-Requested-By": session.session_resource.session.id,
+						"Authorization":  "Bearer otmmToken " + session.session_resource.session.message_digest,
+						"otmmauthtoken":  session.session_resource.session.message_digest
+					}
+				});
+	}
+
+	/**
+	* Retrieve all recent Asset
+	 * <ul>
+	 * 	<li>Method: GET</li>
+	 * 	<li>API method: /v6/assets/recent</li>
+	 * 	<li>URL example: https://developer.opentext.com/otmmapi/v6/assets/recent</li>
+	 * </ul>	
+	*/
+	static async retrieveAllRecentAssets(){
+	}
+
+	
+	/**
+	 * Get list of collections for current user
+	 * @see <a href="https://masteringjs.io/tutorials/axios/headers">Setting Request Headers with Axios</a>
+	 */
+	static async getListOfCollectionsForCurrentUser(session){
+		try {		
+			let link = urlBase + "/v6/collections";
+			console.log("URL: " + link);
+			
+			let result = await OTMMAPI.get(session, link);					
+			
+			return result.data;
+		} catch (error) {
+			console.error("Error: " + error);
+			return null;
+		}
+	}
+}
+
+
+OTMMAPI.createSession(user, pass).then( session => {
+    console.log(session);
+	
+	OTMMAPI.getListOfCollectionsForCurrentUser(session).then(collections => {
+		console.log( JSON.stringify(collections) );
+	});	
+});
