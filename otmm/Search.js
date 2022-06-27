8ba6810279156528a7c840d5dc9642aca3bec92c
JoaquinOnSoft <joaquingp@gmail.com>
2022-06-27 15:31:28 +0200

    Test improved

--- otmm/Search.js
+++ otmm/Search.js
@@ -11,21 +11,22 @@ export default class Search extends OTMMAPI {
 	 * 	<li>URL example: https://developer.opentext.com/otmmapi/v6/search/text</li>
 	 * </ul>
 	 */
-	static async performTextBasedSearch(session, keywords){
+	static async performTextBasedSearch(session, keywords, after=0,limit= 200){
 		try {		
 			let link = this.urlBase + "/v6/search/text";
 			console.log("URL: " + link);
+			console.log(`Params: keyword: ${keywords}, after: ${after}, limit: ${limit}`);
 			
 			let params ={
 				"keyword_query": keywords,
 				"load_type": "metadata",
 				"load_multilingual_values": true,
 				"level_of_detail": "slim",
-				"after": 0,
-				"limit": 200,
+				"after": after,
+				"limit": limit,
 				"multilingual_language_code": "en_US",
 				//"search_config_id": 3,
-				//1 - Metadata, 2 - File Content, 3 - Metadata and Content
+				//keyword_filter_scope_id: 1 - Metadata, 2 - File Content, 3 - Metadata and Content
 				"keyword_scope_id": 3,
 				"preference_id": "ARTESIA.PREFERENCE.GALLERYVIEW.DISPLAYED_FIELDS",
 				"metadata_to_return": "ARTESIA.FIELD.TAG",
@@ -33,7 +34,7 @@ export default class Search extends OTMMAPI {
 			};
 			
 			let result = await OTMMAPI.get(session, link, params);					
-			console.log(result);
+			
 			return result.data;
 		} catch (error) {
 			console.error("Error performTextBasedSearch: " + error);
--- otmm/__test__/Search.test.js
+++ otmm/__test__/Search.test.js
@@ -23,3 +23,26 @@ test('Search - Perform a text based Search', async () => {
 	expect(results.search_result_resource.search_result.facet_field_response_list.length).toBeGreaterThan(0);	
 });  
 
+test('Search - Perform a text based Search (after 5)', async () => {		
+	var results = await Search.performTextBasedSearch(session, "London", 5);
+	expect(results).not.toBe(null);
+	expect(results.search_result_resource).not.toBe(null);
+	expect(results.search_result_resource.search_result).not.toBe(null);
+	expect(results.search_result_resource.search_result.asset_id_list).not.toBe(null);
+	expect(results.search_result_resource.search_result.asset_id_list.length).toBeGreaterThan(0);
+	expect(results.search_result_resource.search_result).not.toBe(null);
+	expect(results.search_result_resource.search_result.facet_field_response_list).not.toBe(null);	
+	expect(results.search_result_resource.search_result.facet_field_response_list.length).toBeGreaterThan(0);	
+});  
+
+test('Search - Perform a text based Search (after 5 and limit 10)', async () => {		
+	var results = await Search.performTextBasedSearch(session, "London", 5, 10);
+	expect(results).not.toBe(null);
+	expect(results.search_result_resource).not.toBe(null);
+	expect(results.search_result_resource.search_result).not.toBe(null);
+	expect(results.search_result_resource.search_result.asset_id_list).not.toBe(null);
+	expect(results.search_result_resource.search_result.asset_id_list.length).toBe(10);
+	expect(results.search_result_resource.search_result).not.toBe(null);
+	expect(results.search_result_resource.search_result.facet_field_response_list).not.toBe(null);	
+	expect(results.search_result_resource.search_result.facet_field_response_list.length).toBeGreaterThan(0);	
+});  
