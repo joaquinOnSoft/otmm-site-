918d17f6a4e044ee64a645ebf89df8e80e0f6328
JoaquinOnSoft <joaquingp@gmail.com>
2023-01-17 18:00:45 +0100

    New component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AssetsListComponent } from './components/assets-list/assets-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssetsListComponent
   ],
   imports: [
     BrowserModule
